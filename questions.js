c85d069c303aafd920261eb9b793301d6822a0d5
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-01-25 15:08:23 +0200

    Add list of questions and options

---
+++ questions.js
@@ -0,0 +1,68 @@
+const questions = [
+  {
+    numb: 1,
+    question: 'What does HTML stand for?',
+    answer: 'Hyper Text Markup Language',
+    options: [
+      'Hyper Text Preprocessor',
+      'Hyper Text Markup Language',
+      'Hyper Text Multiple Language',
+      'Hyper Tool Multi Language',
+    ],
+  },
+  {
+    numb: 2,
+    question: 'What does CSS stand for?',
+    answer: 'Cascading Style Sheet',
+    options: [
+      'Common Style Sheet',
+      'Colorful Style Sheet',
+      'Computer Style Sheet',
+      'Cascading Style Sheet',
+    ],
+  },
+  {
+    numb: 3,
+    question: 'What does PHP stand for?',
+    answer: 'Hypertext Preprocessor',
+    options: [
+      'Hypertext Preprocessor',
+      'Hypertext Programming',
+      'Hypertext Preprogramming',
+      'Hometext Preprocessor',
+    ],
+  },
+  {
+    numb: 4,
+    question: 'What does XML stand for?',
+    answer: 'eXtensible Markup Language',
+    options: [
+      'eXtensible Markup Language',
+      'eXecutable Multiple Language',
+      'eXTra Multi-Program Language',
+      'eXamine Multiple Language',
+    ],
+  },
+  {
+    numb: 5,
+    question: 'What is react?',
+    answer: 'JavaScript Library',
+    options: [
+      'JavaScript Library',
+      'It is a framework',
+      'It is text Editor',
+      'It is a method to write code',
+    ],
+  },
+  {
+    numb: 7,
+    question: 'What are arrow functions?',
+    answer: 'All other answers are correct',
+    options: [
+      'Arrow functions were introduced in the ES6 version of javascript.',
+      'They provide us with a new and shorter syntax for declaring functions.',
+      'All other answers are correct',
+      'Arrow functions can only be used as a function expression.',
+    ],
+  },
+];
